d19d26661dd8aa30c8d662c53042fc2ea87c48ae
Adrian Petre <darthkojones@gmail.com>
2023-11-19 20:35:38 +0100

    Update Exam Prep.py

--- Exam Prep.py
+++ Exam Prep.py
@@ -85,3 +85,92 @@ def smallest_multiple(n):
 
 # Call the function with 20 as the input and print the result
 print(f"Problem 5 - The smallest number evenly divisible by all of the numbers from 1 to 20 is: {smallest_multiple(20)}")
+
+
+# Problem 6:
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+def sum_of_squares(n):
+    return sum(i**2 for i in range(1, n + 1))  # Calculate the sum of the squares of the first n natural numbers
+
+def square_of_sum(n):
+    return sum(range(1, n + 1)) ** 2  # Calculate the square of the sum of the first n natural numbers
+
+def difference(n):
+    return square_of_sum(n) - sum_of_squares(n)  # Calculate the difference between the square of the sum and the sum of the squares
+
+# Call the function with 100 as the input and print the result
+print(f"Problem 6 - The difference is: {difference(100)}")
+
+
+# Problem 7:
+# Find the 10,001st prime number.
+
+def is_prime(n):
+    if n <= 1:  # Numbers less than or equal to 1 are not prime
+        return False
+    if n <= 3:  # 2 and 3 are prime numbers
+        return True
+    if n % 2 == 0 or n % 3 == 0:  # Eliminate multiples of 2 and 3
+        return False
+    i = 5
+    while i * i <= n:  # Check only up to the square root of n
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6  # Increase i by 6, since we already checked multiples of 2 and 3
+    return True
+
+def find_nth_prime(n):
+    count = 0  # Counter for the number of primes found
+    prime = 1  # Start checking for primes from number 1
+
+    while count < n:  # Continue until n primes are found
+        prime += 1  # Check the next number
+        if is_prime(prime):
+            count += 1  # Increment the count if a prime is found
+
+    return prime  # Return the nth prime number
+
+# Call the function with 10001 as the input and print the result
+print(f"Problem 7 - The 10001st prime number is: {find_nth_prime(10001)}")
+
+
+
+# Problem 8:
+# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product.
+
+# The 1000-digit number
+number = (
+    "73167176531330624919225119674426574742355349194934"
+    "96983520312774506326239578318016984801869478851843"
+    "85861560789112949495459501737958331952853208805511"
+    "12540698747158523863050715693290963295227443043557"
+    "66896648950445244523161731856403098711121722383113"
+    "62229893423380308135336276614282806444486645238749"
+    "30358907296290491560440772390713810515859307960866"
+    "70172427121883998797908792274921901699720888093776"
+    "65727333001053367881220235421809751254540594752243"
+    "52584907711670556013604839586446706324415722155397"
+    "53697817977846174064955149290862569321978468622482"
+    "83972241375657056057490261407972968652414535100474"
+    "82166370484403199890008895243450658541227588666881"
+    "16427171479924442928230863465674813919123162824586"
+    "17866458359124566529476545682848912883142607690042"
+    "24219022671055626321111109370544217506941658960408"
+    "07198403850962455444362981230987879927244284909188"
+    "84580156166097919133875499200524063689912560717606"
+    "05886116467109405077541002256983155200055935729725"
+    "71636269561882670428252483600823257530420752963450"
+)
+
+def greatest_product(num, length):
+    max_product = 0  # Initialize the maximum product to 0
+    for i in range(len(num) - length + 1):  # Iterate over the number, considering groups of 'length' digits
+        product = 1  # Initialize product for the current group of digits
+        for j in range(length):  # Iterate over each digit in the group
+            product *= int(num[i + j])  # Multiply the digits together
+        max_product = max(max_product, product)  # Update the maximum product if the current product is greater
+    return max_product  # Return the greatest product found
+
+# Call the function with the 1000-digit number and 13 as the length and print the result
+print(f"Problem 8 - The greatest product of thirteen adjacent digits is: {greatest_product(number, 13)}")
